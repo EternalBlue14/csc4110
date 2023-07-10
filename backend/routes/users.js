911fa382e053fe5434f58830ec25be794c2b2b5f
Derrick-51 <95022335+Derrick-51@users.noreply.github.com>
2023-07-09 21:47:27 -0400

    Add quiz routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,7 +4,12 @@ const {
     getUser,
     getUsers, 
     deleteUser,
-    updateUser
+    updateUser,
+    getQuiz,
+    getQuizzes,
+    createQuiz,
+    deleteQuiz,
+    updateQuiz
 } = require('../controllers/userController')
 
 const router = express.Router()
@@ -23,5 +28,20 @@ router.delete('/:id', deleteUser)
 
 // UPDATE a user
 router.patch('/:id', updateUser)
+
+// GET a single quiz
+router.get('/:id/quiz/:id', getQuiz)
+
+// GET all quizzes
+router.get('/:id/quiz', getQuizzes)
+
+// POST a new quiz
+router.post('/:id/quiz/:id', createQuiz)
+
+// DELETE a quiz
+router.delete('/:id/quiz/:id', deleteQuiz)
+
+// UPDATE a quiz
+router.patch('/:id/quiz/:id', updateQuiz)
 
 module.exports = router
