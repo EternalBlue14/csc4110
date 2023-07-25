fb873b706bb8bb545c69962bcd4315d296897d5e
Spencerml29 <Spencerml29@gmail.com>
2023-07-25 01:44:11 -0400

    Logout.css added

--- frontend/src/pages/Logout.jsx
+++ frontend/src/pages/Logout.jsx
@@ -1,7 +1,9 @@
+// Logout.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
 import { useAuthContext } from '../hooks/useAuthContext';
+import './Logout.css';
 
 export default function Logout() {
   const { logout } = useLogout();
@@ -12,41 +14,11 @@ export default function Logout() {
   };
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '50vh',
-        width: '157vh'
-      }}
-    >
-      <div
-        style={{
-          textAlign: 'center',
-          maxWidth: '300px',
-          padding: '80px',
-          border: '3px solid #000000',
-          borderRadius: '5px',
-          backgroundColor: '#EBCCAB', // Tan color
-        }}
-      >
-        <h2 style={{ marginBottom: '40px' }}>Hello, {user.userName}!</h2>
-        <p style={{ marginBottom: '50px', fontSize: '21px', }}>Are you sure you want to sign out?</p>
-        <button
-          onClick={handleClick}
-          style={{
-            padding: '24px 48px',
-            fontSize: '20px',
-            fontWeight: 'bold',
-            color: 'white',
-            backgroundColor: '#007bff',
-            cursor: 'pointer',
-          }}
-        >
-          Sign Out
-        </button>
+    <div className="container">
+      <div className="logout-box">
+        <h2>Hello, {user.userName}!</h2>
+        <p>Are you sure you want to sign out?</p>
+        <button onClick={handleClick}>Sign Out</button>
       </div>
     </div>
   );
